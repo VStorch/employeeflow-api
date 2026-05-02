e739f23a7da5d348c120a5c0200661799a6b5f38
Vinícius <viniciusstorch62@gmail.com>
2026-05-02 09:24:02 -0300

    feat(entities): add Employee class

---
+++ Entities/Employee.cs
@@ -0,0 +1,9 @@
+namespace EmployeeFlow.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
